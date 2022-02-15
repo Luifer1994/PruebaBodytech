eee6459d177304869407ddac9aae6059ec0a9908
luifer Almendrales <almendralesluifer@gmail.com>
2022-02-15 12:52:13 -0500

    list products paginate

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $request["limit"] ? $limit = $request["limit"] : $limit = 10;
+        $products = Product::paginate($limit);
+
+        return response()->json([
+            "res" => true,
+            "data" => $products
+        ]);
+    }
 }
