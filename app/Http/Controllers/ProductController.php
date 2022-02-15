94230f4bd4d3c7968e34693a5ef37b2162d3a57e
luifer Almendrales <almendralesluifer@gmail.com>
2022-02-15 13:14:24 -0500

    update products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,29 +11,59 @@ class ProductController extends Controller
     public function index(Request $request)
     {
         $request["limit"] ? $limit = $request["limit"] : $limit = 10;
+
         $products = Product::paginate($limit);
 
         return response()->json([
-            "res" => true,
-            "data" => $products
+            "res"   => true,
+            "data"  => $products
         ], 200);
     }
 
     public function store(ProductRequest $request)
     {
         $newProduct = new Product();
-        $newProduct->name = $request->name;
-        $newProduct->price = $request->price;
+
+        $newProduct->name   = $request->name;
+        $newProduct->price  = $request->price;
 
         if ($newProduct->save()) {
             return response()->json([
-                "res" => true,
-                "message" => "Registro exitoso"
+                "res"       => true,
+                "message"   => "Registro exitoso"
             ], 200);
         } else {
             return response()->json([
-                "res" => false,
-                "message" => "Error al guardar registro"
+                "res"       => false,
+                "message"   => "Error al guardar registro"
+            ], 400);
+        }
+    }
+
+    public function update(ProductRequest $request, $id)
+    {
+        $product = Product::find($id);
+
+        if ($product) {
+
+            $product->name  = $request->name;
+            $product->price = $request->price;
+
+            if ($product->update()) {
+                return response()->json([
+                    "res"       => true,
+                    "message"   => "Actualización exitosa"
+                ], 200);
+            } else {
+                return response()->json([
+                    "res"       => false,
+                    "message"   => "Error al actualizar el registro"
+                ], 400);
+            }
+        } else {
+            return response()->json([
+                "res"       => false,
+                "message"   => "El producto a actualizar no existe"
             ], 400);
         }
     }
--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::controller(ProductController::class)->group(function () {
     Route::get('products-list{limit?}', 'index');
     Route::post('products-store', 'store');
+    Route::put('products-update/{id}', 'update');
 });
